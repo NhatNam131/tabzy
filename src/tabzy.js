d901134a320aba47b6c1e4b35074aad52e87c808
NNhatNam99 <namnnph06519@gmail.gmailcom>
2025-02-19 18:25:40 +0700

    Handle switch tab and display respectively content

--- src/tabzy.js
+++ src/tabzy.js
@@ -24,4 +24,35 @@ function Tabzy(selector) {
     .filter(Boolean);
 
   if (this.panels.length !== this.tabs.length) return;
+
+  this._init();
 }
+
+Tabzy.prototype._init = function () {
+  const tabActive = this.tabs[0];
+  tabActive.closest("li").classList.add("tabzy--active");
+
+  this.panels.forEach((panel) => (panel.hidden = true));
+
+  this.tabs.forEach((tab) => {
+    tab.onclick = (e) => this._handleTabClick(e, tab);
+  });
+
+  const panelActive = this.panels[0];
+  panelActive.hidden = false;
+};
+
+Tabzy.prototype._handleTabClick = function (e, tab) {
+  e.preventDefault();
+
+  this.tabs.forEach((tab) => {
+    tab.closest("li").classList.remove("tabzy--active");
+  });
+
+  tab.closest("li").classList.add("tabzy--active");
+
+  this.panels.forEach((panel) => (panel.hidden = true));
+
+  const panelActive = document.querySelector(tab.getAttribute("href"));
+  panelActive.hidden = false;
+};
