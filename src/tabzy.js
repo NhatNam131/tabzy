448ea818f28a789bad60caeabc072a36ab53b0b9
NNhatNam99 <namnnph06519@gmail.gmailcom>
2025-02-21 00:40:08 +0700

    Clean code and create switch method

--- src/tabzy.js
+++ src/tabzy.js
@@ -25,26 +25,12 @@ function Tabzy(selector) {
 
   if (this.panels.length !== this.tabs.length) return;
 
+  this._originalHTML = this.container.innerHTML;
+
   this._init();
 }
 
-Tabzy.prototype._init = function () {
-  const tabActive = this.tabs[0];
-  tabActive.closest("li").classList.add("tabzy--active");
-
-  this.panels.forEach((panel) => (panel.hidden = true));
-
-  this.tabs.forEach((tab) => {
-    tab.onclick = (e) => this._handleTabClick(e, tab);
-  });
-
-  const panelActive = this.panels[0];
-  panelActive.hidden = false;
-};
-
-Tabzy.prototype._handleTabClick = function (e, tab) {
-  e.preventDefault();
-
+Tabzy.prototype._activeTab = function (tab) {
   this.tabs.forEach((tab) => {
     tab.closest("li").classList.remove("tabzy--active");
   });
@@ -55,4 +41,48 @@ Tabzy.prototype._handleTabClick = function (e, tab) {
 
   const panelActive = document.querySelector(tab.getAttribute("href"));
   panelActive.hidden = false;
+};
+
+Tabzy.prototype._handleTabClick = function (e, tab) {
+  e.preventDefault();
+
+  this._activeTab(tab);
+};
+
+Tabzy.prototype._init = function () {
+  this._activeTab(this.tabs[0]);
+
+  this.tabs.forEach((tab) => {
+    tab.onclick = (e) => this._handleTabClick(e, tab);
+  });
+};
+
+Tabzy.prototype.switch = function (input) {
+  let toActive = null;
+
+  if (typeof input === "string") {
+    toActive = this.tabs.find((tab) => tab.getAttribute("href") === input);
+
+    if (!toActive) {
+      console.error(`Tabzy: No panel found with ID ${input}`);
+      return;
+    }
+  } else if (this.tabs.includes(input)) {
+    toActive = input;
+  }
+
+  if (!toActive) {
+    console.error(`Tabzy: Invalid input '${input}`);
+    return;
+  }
+
+  this._activeTab(toActive);
+};
+
+Tabzy.prototype.destroy = function () {
+  this.container.innerHTML = this._originalHTML;
+  this.panels.forEach((panel) => (panel.hidden = false));
+  this.container = null;
+  this.tabs = null;
+  this.panels = null;
 };
